2b1b34196019fb29d1b927747a8743515c5281ad
retsambew <namanaskn2@gmail.com>
2023-04-07 21:13:23 +0530

    Problem 1876

---
+++ Sliding Window/DistinctThreeSubset.java
@@ -0,0 +1,30 @@
+/*
+ * 1876. Substrings of Size Three with Distinct Characters
+ * Link https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
+ */
+
+class Solution {
+    public static int countGoodSubstrings(String s) {
+        int ans=0;
+        for(int i=0;i<s.length()-2;i++){
+            if(!(s.charAt(i)==s.charAt(i+1) || s.charAt(i) == s.charAt(i+2) || s.charAt(i+1) == s.charAt(i+2)))
+                ans++;
+        }
+        return ans;
+    }
+
+    // 
+    // public static int countGoodSubstrings(String s) {
+    //     int ans=0;
+    //     for(int i=0;i<s.length()-2;i++){
+    //         Set<Character> set=new HashSet<Character>();
+    //         for(int j=i;j<i+3;j++){
+    //             if(!set.add(s.charAt(j)))
+    //                 break;
+    //         }
+    //         if(set.size()==3)
+    //             ans++;
+    //     }
+    //     return ans;
+    // }
+}
