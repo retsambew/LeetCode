357cdb567f7f9d238ab98715ea1ccce597a6a0e9
retsambew <namanaskn2@gmail.com>
2023-04-11 09:44:37 +0530

    P 643

--- Sliding Window/MaxSumSubarray2.java
+++ Sliding Window/MaxSumSubarray2.java
@@ -5,6 +5,9 @@
 
 public class MaxSumSubarray2 {
     public long maximumSubarraySum(int[] nums, int k) {
-        
+        int sum=0;
+        for (int i = 0; i < k; i++) {
+            sum+=nums[i];
+        }
     }
 }
