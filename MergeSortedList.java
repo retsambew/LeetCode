4e5d3bc63df550037156b18279fcb352c6dd7572
retsambew <namanaskn2@gmail.com>
2023-04-14 01:49:35 +0530

    P21 recursive

--- MergeSortedList.java
+++ MergeSortedList.java
@@ -11,33 +11,44 @@ class ListNode {
 }
 
 public class MergeSortedList {
-    // iterative solution
+    // recursive solution
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1==null) return list2;
         if(list2==null) return list1;
 
-        ListNode list=new ListNode();
-        ListNode head=list;
-        while(list1!=null || list2!=null){
-            if(list1==null){
-                list.next=list2;
-                break;
-            }
-            if(list2==null){
-                list.next=list1;
-                break;
-            }
-            if(list1.val<list2.val){
-                list.next=new ListNode(list1.val,null);
-                list1=list1.next;
-                list=list.next;
-            }
-            else{
-                list.next=new ListNode(list2.val,null);
-                list2=list2.next;
-                list=list.next;
-            }
+        if(list1.val<list2.val){
+            return new ListNode(list1.val,mergeTwoLists(list1.next,list2));
         }
-        return head.next;
+        return new ListNode(list2.val,mergeTwoLists(list1,list2.next));
     }
+
+    // iterative solution
+    // public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    //     if(list1==null) return list2;
+    //     if(list2==null) return list1;
+
+    //     ListNode list=new ListNode();
+    //     ListNode head=list;
+    //     while(list1!=null || list2!=null){
+    //         if(list1==null){
+    //             list.next=list2;
+    //             break;
+    //         }
+    //         if(list2==null){
+    //             list.next=list1;
+    //             break;
+    //         }
+    //         if(list1.val<list2.val){
+    //             list.next=new ListNode(list1.val,null);
+    //             list1=list1.next;
+    //             list=list.next;
+    //         }
+    //         else{
+    //             list.next=new ListNode(list2.val,null);
+    //             list2=list2.next;
+    //             list=list.next;
+    //         }
+    //     }
+    //     return head.next;
+    // }
 }
