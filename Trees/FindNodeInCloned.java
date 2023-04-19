8bb658069bc3157ba3c1c38e26bd7bfdc742caab
retsambew <namanaskn2@gmail.com>
2023-04-19 14:09:18 +0530

    P1379

---
+++ Trees/FindNodeInCloned.java
@@ -0,0 +1,28 @@
+/*
+ * 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+ */
+
+public class FindNodeInCloned {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == target)
+            return cloned;
+        if (original.left != null) {
+            TreeNode t = getTargetCopy(original.left, cloned.left, target);
+            if (t != null)
+                return t;
+        }
+        if (original.right != null)
+            return getTargetCopy(original.right, cloned.right, target);
+        return null;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
