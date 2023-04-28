bea67a0d322005249dd325c43238ac581957dcee
retsambew <namanaskn2@gmail.com>
2023-04-28 22:23:51 +0530

    P69

---
+++ Binary Search/Sqrt.java
@@ -0,0 +1,13 @@
+public class Sqrt {
+    // TLE
+    // public int mySqrt(int x) {
+    // for (int i = 0; i <= x; i++) {
+    // System.out.println(i * i);
+    // if (i * i == x)
+    // return i;
+    // else if (i * i > x)
+    // return i - 1;
+    // }
+    // return 0;
+    // }
+}
