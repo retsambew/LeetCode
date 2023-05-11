42cb1142d8222f4f38bfc164b35d7ffb1e86982e
retsambew <namanaskn2@gmail.com>
2023-05-11 10:50:59 +0530

    P222

---
+++ Trees/NodeCount.java
@@ -0,0 +1,11 @@
+/*
+ * 222. Count Complete Tree Nodes
+ */
+
+public class NodeCount {
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
