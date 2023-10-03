d716b5159a588847ca86dfe152ed77264595ff6c
retsambew <namanaskn2@gmail.com>
2023-10-03 11:12:32 +0530

    day 3 oct 23

---
+++ Oct23/Day3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans = 0;
+
+        // filling the map to know count of number of occurences of elements.
+        for(int i: nums)
+            map.put(i, map.getOrDefault(i,0)+1);
+
+        // using the formula to calculate total number of pairs per unique element.
+        for(Map.Entry<Integer,Integer> e: map.entrySet()){
+            int n = e.getValue() - 1;
+            ans+=((n*(n+1))/2); // sum of n natural numbers
+        }
+        return ans;
+    }
+}
