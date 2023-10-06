f3b6d7b292b8807e9cacbd15f86519aa2c69eb14
retsambew <namanaskn2@gmail.com>
2023-10-06 11:12:32 +0530

    day 6 oct 23

---
+++ Oct23/Day6.java
@@ -0,0 +1,28 @@
+class Solution {
+    /* Tabulation */
+    public int integerBreak(int n) {
+        int dp[] = new int[59];
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            int maxi = 1;
+            for(int j=1; j<i; j++){
+                maxi = Math.max(maxi, j * Math.max(i-j, dp[i-j]));
+            }
+            dp[i] = maxi;
+        }
+        return dp[n];
+    }
+
+    /* Memoization */
+    // int dp[] = new int[59];
+    // public int integerBreak(int n) {
+    //     if(n==1) return 1;
+    //     int ans = 1;
+    //     for(int i=1; i<n; i++){
+    //         if(dp[n-i]==0)
+    //             dp[n-i]=integerBreak(n-i);
+    //         ans = Math.max(ans, i * Math.max(n-i,dp[n-i]));
+    //     }
+    //     return ans;
+    // }
+}
