3a3057f94e7b632e11bbafa9aa189215791511e6
retsambew <namanaskn2@gmail.com>
2023-10-06 16:42:36 +0530

    p343 tabulation

--- DP/IntegerBreak.java
+++ DP/IntegerBreak.java
@@ -3,15 +3,30 @@
  */
 
 class Solution {
-    int dp[] = new int[59];
+    /* Tabulation */
     public int integerBreak(int n) {
-        if(n==1) return 1;
-        int ans = 1;
-        for(int i=1; i<n; i++){
-            if(dp[n-i]==0)
-                dp[n-i]=integerBreak(n-i);
-            ans = Math.max(ans, i * Math.max(n-i,dp[n-i]));
+        int dp[] = new int[59];
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            int maxi = 1;
+            for(int j=1; j<i; j++){
+                maxi = Math.max(maxi, j * Math.max(i-j, dp[i-j]));
+            }
+            dp[i] = maxi;
         }
-        return ans;
+        return dp[n];
     }
+
+    /* Memoization */
+    // int dp[] = new int[59];
+    // public int integerBreak(int n) {
+    //     if(n==1) return 1;
+    //     int ans = 1;
+    //     for(int i=1; i<n; i++){
+    //         if(dp[n-i]==0)
+    //             dp[n-i]=integerBreak(n-i);
+    //         ans = Math.max(ans, i * Math.max(n-i,dp[n-i]));
+    //     }
+    //     return ans;
+    // }
 }
