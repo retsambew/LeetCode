096b744585ac69c8a53c9b91148440e10b6d67d9
retsambew <namanaskn2@gmail.com>
2023-10-14 11:12:32 +0530

    day 14 oct 23

--- Oct23/Day14.java
+++ Oct23/Day14.java
@@ -9,6 +9,7 @@ class Solution {
             for (int j = n; j > 0; --j) 
                 dp[j] = Math.min(dp[j], dp[Math.max(j-time[i]-1, 0)]+cost[i]);
         }
+        
         return dp[n];
     }
 }
