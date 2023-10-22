53ed1caa4cc81ec1dc916e2ce5f551e705c8fa4a
retsambew <namanaskn2@gmail.com>
2023-10-22 11:12:32 +0530

    day 22 oct 23

---
+++ Oct23/Day23.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n == 1) return true;
+        if (n <= 0) return false;
+
+        double logarithmBase4 = Math.log(n) / Math.log(4);
+        return (logarithmBase4 == (int)logarithmBase4);
+    }
+}
